89c9910f26b6cbe0b5b3194b99874a5f99a61ec2
beeeg55 <beeeg@naver.com>
2020-05-27 22:35:36 +0900

    Solve p_12925 문자열을 정수로 바꾸기

---
+++ src/연습문제/p_12925_문자열을정수로바꾸기.java
@@ -0,0 +1,13 @@
+package 연습문제;
+
+public class p_12925_문자열을정수로바꾸기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
--- src/연습문제/p_12928_약수의합.java
+++ src/연습문제/p_12928_약수의합.java
@@ -4,7 +4,7 @@ public class p_12928_약수의합 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(solution);
+		
 	}
 	public int solution(int n) {
 		 int answer = n;
